54629270f1011f1e91817bba300a3ba08808b773
alireza.azizi <alireza.azizi@pnr.ir>
2025-11-15 19:02:56 +0330

    html template added

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.exceptions import HTTPException as starlette_HTTPException
 
 from app.db import lifespan
 from app.routes import router
@@ -13,3 +16,19 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+templates = Jinja2Templates(
+    directory=r"template\404.html"
+)
+
+
+@app.exception_handler(starlette_HTTPException)
+async def custom_http_exception_handler(request: Request, exc: starlette_HTTPException):
+    if exc.status_code == 404:
+        return templates.TemplateResponse(
+            "404.html",
+            {"request": request, "path": request.url},
+            status_code=404,
+        )
+    return HTMLResponse(str(exc.detail), status_code=exc.status_code)
